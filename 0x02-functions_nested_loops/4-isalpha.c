cb03a511af72df82235376027c6a87ae35aed854
MedaxCD <89552832+MedaxCD@users.noreply.github.com>
2022-07-05 21:41:28 -0400

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
-* _isalpha-checks for alphabetical letters
-* @c:a charter to be checke on
+* _isalpha - checks for alphabetical letters
+* @c: a charter to be checke on
 * Return: returns 0 or 1 depending on condition
 */
 int _isalpha(int c)
