6e40880a15cda1d118039b722c3ed86d1488ad44
MedaxCD <89552832+MedaxCD@users.noreply.github.com>
2022-07-05 21:32:41 -0400

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,19 +1,11 @@
 #include "main.h"
 
 /**
- * print_last_digit - function that prints the last digit of a number.
- * @x: The input number to check
- * Return: int.
- */
-
-int print_last_digit(int x)
+* _isalpha - checks for alphabetical letters
+* @c: a charter to be checke on
+* Return: returns 0 or 1 depending on condition
+*/
+int _isalpha(int c)
 {
-
-	x %= 10;
-
-	if (x < 0)
-		x *= -1;
-
-	_putchar(x + '0');
-	return (x);
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
