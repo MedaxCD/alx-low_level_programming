03fab4fb8d7e1a4325c306906a32687a1c3fb537
MedaxCD <89552832+MedaxCD@users.noreply.github.com>
2022-07-15 00:11:08 -0400

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
-#include <ctype.h>
 
 /**
- * main - program that prints the alphabet in lowercase, followed by a new line
- * You can only use the putchar
- * Return: 0
+ *main -> assign a random number to the variable n each time it is executed
+ *and print the last digit of the number stored in the variable n
+ *Return: Always 0 (Success)
  */
+int main(void)
+{
+	int ch;
 
-int main(void){
-    int l = 'a';
-    while (l <= 'z')
-    {
-        putchar(l);
-        l += 1;
-    }
-    putchar('\n');
-    return (0);
+	for (ch = 97; ch <= 122; ch++)
+	{
+		putchar(ch);
+	}
+	putchar(10); /* this is an ascii code for new line*/
+
+	return (0);
 }
