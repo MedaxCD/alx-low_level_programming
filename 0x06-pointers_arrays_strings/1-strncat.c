0879fd32e5d0dccf437446abe7da56b67f70eed9
MedaxCD <89552832+MedaxCD@users.noreply.github.com>
2022-07-14 22:06:01 -0400

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,14 +2,14 @@
 #include <string.h>
 
 /**
- * * _strncat -> function to append some charx
- * * @dest: first param
- * * @src: second param
- * * @n: 3rd param
- * * Return: string
- * */
+* _strncat -> function to append some charx
+* @dest: first param
+* @src: second param
+* @n: 3rd param
+* Return: string
+*/
 char *_strncat(char *dest, char *src, int n)
 {
-		strncat(dest, src, n);
-			return (dest);
+	strncat(dest, src, n);
+	return (dest);
 }
